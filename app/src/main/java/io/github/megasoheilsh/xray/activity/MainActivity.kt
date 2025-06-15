5116d7d42cb770eec3ec6d61df297b0f9fd3b1d7
megasoheilsh <soheilshokrollahinia@gmail.com>
2025-06-15 21:13:20 +0330

    v10.9.0

--- app/src/main/java/io/github/megasoheilsh/xray/activity/MainActivity.kt
+++ app/src/main/java/io/github/megasoheilsh/xray/activity/MainActivity.kt
@@ -43,6 +43,7 @@ import io.github.megasoheilsh.xray.dto.ProfileList
 import io.github.megasoheilsh.xray.helper.HttpHelper
 import io.github.megasoheilsh.xray.helper.LinkHelper
 import io.github.megasoheilsh.xray.helper.ProfileTouchHelper
+import io.github.megasoheilsh.xray.helper.UpdateHelper
 import io.github.megasoheilsh.xray.service.TProxyService
 import io.github.megasoheilsh.xray.viewmodel.LinkViewModel
 import io.github.megasoheilsh.xray.viewmodel.ProfileViewModel
